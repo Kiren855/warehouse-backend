95a17339134344d013d8273519c634f447fa7afd
HUAN <kiren10122003@gmail.com>
2025-09-02 05:14:42 +0700

    create create-group-request

--- identity-service/src/main/java/com/sunny/scm/identity/dto/group/CreateGroupRequest.java
+++ identity-service/src/main/java/com/sunny/scm/identity/dto/group/CreateGroupRequest.java
@@ -1,5 +1,8 @@
 package com.sunny.scm.identity.dto.group;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
@@ -8,4 +11,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreateGroupRequest {
 
+    @NotBlank(message = "Group name must not be blank")
+    @JsonProperty("group_name")
+    String groupName;
 }
